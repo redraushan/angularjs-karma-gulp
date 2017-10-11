ba3ff4d242e45c18aea39475c4bc3a42085045c5
Raushan Sharma <raushan@XEBIAINDIA.LOCAL>
2017-10-11 15:52:10 +0530

    Initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,8 @@ module.exports = function(config) {
     browsers: ['Chrome'],
     frameworks: ['jasmine'],
     files: [
+      './node_modules/angular/angular.js',
+      './node_modules/angular-mocks/angular-mocks.js',
       'src/**/*.js',
       'test/**/*.spec.js'
     ]
